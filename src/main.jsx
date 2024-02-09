f28582c15d16b7ec33705ffff8fc6eea2b7d609f
samidss56 <adimas.id@gmail.com>
2024-02-09 16:29:17 +0700

    page routing using react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import LoginPage from "./pages/login.jsx";
+import RegisterPage from "./pages/register.jsx";
+import ErrorPage from "./pages/404.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Helloworld</div>,
+    errorElement: <ErrorPage/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,10 +1,17 @@
 import FormRegister from "../components/Fragments/FormRegister";
 import AuthLayouts from "../components/Layouts/AuthLayouts";
+import { Link } from "react-router-dom";
 
 const RegisterPage = () => {
   return (
     <AuthLayouts title="Register">
       <FormRegister />
+      <p className="text-sm mt-5 text-center">
+        Already have an account?{" "}
+        <Link to="/login" className="font-bold text-blue-600">
+          Sign in here
+        </Link>
+      </p>
     </AuthLayouts>
   );
 };
